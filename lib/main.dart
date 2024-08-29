16221451a76b2b8147db0ffdc632304692cf6390
Renan Pereira Volpe <renanvolpe067@gmail.com>
2024-08-29 17:41:51 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:classroom_project/app_module.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() async {
+  runApp(ModularApp(
+    module: AppModule(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Classroom App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      routerConfig: Modular.routerConfig,
+    );
+  }
+}
